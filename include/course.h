90a5eca1bc55862f51084a95cd3a4e56db78ed61
jazminbelmonte <jazminbelmonte@gmail.com>
2018-11-28 21:38:40 -0700

    rough work

--- include/course.h
+++ include/course.h
@@ -1,6 +1,20 @@
 #ifndef _COURSE_H_
 #define _COURSE_H_
 
-//TODO
+#include <string>
+#include <iomanip>
+#include "graph.h"
+
+class Course{
+protected:
+  std::string number, name;
+  int creditHours, size;
+
+public:
+  friend std::ostream& operator<<(std::ostream& out, const Course& c){
+
+    return out;
+  }
+};
 
 #endif
