c836ccb84545da7cf299fb47bbadbb2f989eed55
jazminbelmonte <jazminbelmonte@gmail.com>
2018-11-30 01:03:34 -0700

    trying to create getters and setters

--- include/course.h
+++ include/course.h
@@ -5,16 +5,48 @@
 #include <iomanip>
 #include "graph.h"
 
-class Course{
+class Course {
 protected:
-  std::string number, name;
-  int creditHours, size;
+  int size;
+  std::string number;
+  std::string name;
+  int creditHours;
 
 public:
-  friend std::ostream& operator<<(std::ostream& out, const Course& c){
 
+  friend std::ostream &operator<<(std::ostream &out, const Course &c) {
+    out << std::setw(8) << c.number << " - " << c.name << ": (" << c.creditHours << ")";
     return out;
   }
+
+  //Setters
+  void setSize(int s) {
+    this->size = s;
+  }
+  void setCourseNumber(std::string cnum) {
+    this->number = cnum;
+  }
+  void setCourseName(std::string cn) {
+    this->name = cn;
+  }
+  void setCreditHours(int ch) {
+    this->creditHours = ch;
+  }
+
+  //Getters
+  int getSize() {
+    return size;
+  }
+  std::string getCourseNumber() const{
+    return number;
+  }
+  std::string getCourseName() const{
+    return name;
+  }
+  int getCreditHours() const{
+    return creditHours;
+  }
+
 };
 
 #endif
