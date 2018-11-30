2a81593a05f901b91a8f6272818bc34329147623
jazminbelmonte <jazminbelmonte@gmail.com>
2018-11-30 01:19:11 -0700

    rough main outline

--- src/main.cpp
+++ src/main.cpp
@@ -4,12 +4,23 @@
 
 using namespace std;
 
-void createGraph(Graph<Course>& g) {
-  //TODO
+void createGraph(Graph<Course>* &g) {
+
+  g = new Graph<Course>(10);
+
+  Course("CS 1030", "Foundations of Computing", 4);
+
+
+
+
 }
 
 int main(){
-  //TODO
+  Graph<Course>* g;
+
+  createGraph(g);
+
+  g->printTopologicalOrder();
 
   return 0;
 }
